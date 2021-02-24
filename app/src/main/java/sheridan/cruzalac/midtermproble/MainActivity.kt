c60ce95b958f89d65400d7299a0d0d81391774f8
Arceline Cruz <cruzalacar@outlook.com>
2021-02-24 12:31:07 -0500

    initial commit

---
+++ app/src/main/java/sheridan/cruzalac/midtermproble/MainActivity.kt
@@ -0,0 +1,11 @@
+package sheridan.cruzalac.midtermproble
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
